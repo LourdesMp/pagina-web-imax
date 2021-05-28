300d2f8bdac95451ba299446b531f7133006cd71
LourdesMp <lourdes.merino@imax.com.pe>
2021-05-27 21:58:43 -0500

    editando permisos en el menu nav

--- public/controller-intranet/nav.js
+++ public/controller-intranet/nav.js
@@ -5,28 +5,13 @@ menuMobile.addEventListener('click', () => {
   console.log('click');
 });
 
-// const menuAdmin = document.querySelector('.admin');
-// const navAdmin = document.querySelector('.nav-admin');
-// menuAdmin.addEventListener('click', () => {
-//   navAdmin.classList.toggle('hide');
-//   console.log('click');
-// })
 
-//funcion para ATRAS en lista de colaboradores
-// const btnBack = document.querySelector('.back-worker');
-// const listBoletas = document.querySelector('.container-boletas');
-// const containerBoleta = document.querySelector('.container-add-worker');
-// btnBack.addEventListener('click', () => {
-//   listBoletas.classList.toggle('show');
-//   containerBoleta.classList.toggle('hide');
-//   console.log('click');
-// });
 
 const onlyAdmin = document.querySelector('.only-admin')
 fs.collection('users').onSnapshot((querySnapshot) => {
   onlyAdmin.innerHTML = '';
   querySnapshot.forEach((doc) => {
-    // console.log(`${doc.id} => ${doc.data().checkAdmin}`);
+    console.log(`${doc.id} => ${doc.data().area}`);
     const userLogueado = firebase.auth().currentUser;
 
     if (doc.id === userLogueado.uid) {
@@ -40,50 +25,16 @@ fs.collection('users').onSnapshot((querySnapshot) => {
               <a href="workerAdmin.html" ><li class="btn-admi-const second-parent" >Agregar Colaborador</li></a>
           </ul>
         </a> </li>
-        <li><a href="#/" class="admin-web">Herramientas IMAX
-        <ul class="nav-admin-web hide">
-              <a href="projectsWeb.html" ><li class="btn-admin-boletas second-parent" >Agregar Noticia Web</li></a>
-              <a href="webAddProject.html" ><li class="btn-admi-const second-parent" >Agregar Proyecto </li></a>
-          </ul>
-        </a></li> `;
+       `;
 
-        // const menuAdmin = document.querySelector('.admin');
-        // console.log(menuAdmin)
-        // const navAdmin = document.querySelector('.nav-admin');
-        // menuAdmin.addEventListener('click', () => {
-        //   navAdmin.classList.toggle('hide');
-        //   console.log('click');
-        // })
-
-        // const menuAdminWeb = document.querySelector(".admin-web");
-        // console.log(menuAdminWeb)
-        // const navAdminWeb = document.querySelector(".nav-admin-web");
-        // menuAdminWeb.addEventListener("click", () => {
-        //   navAdminWeb.classList.toggle("hide");
-        //   console.log("click");
-        // });
-      }
-
-      if (doc.data().checkLider === 'SI') {
-        onlyAdmin.innerHTML += `
-        <li><a href="vacationsLider.html">Lider de Unidad</a></li>`;
         const menuAdmin = document.querySelector('.admin');
+        console.log(menuAdmin)
         const navAdmin = document.querySelector('.nav-admin');
         menuAdmin.addEventListener('click', () => {
           navAdmin.classList.toggle('hide');
           console.log('click');
         })
-
-         const menuAdminWeb = document.querySelector(".admin-web");
-         console.log(menuAdminWeb);
-         const navAdminWeb = document.querySelector(".nav-admin-web");
-         menuAdminWeb.addEventListener("click", () => {
-           navAdminWeb.classList.toggle("hide");
-           console.log("click");
-         });
-      }
-
-
+      } 
     }
 
   });
@@ -91,31 +42,87 @@ fs.collection('users').onSnapshot((querySnapshot) => {
 })
 
 
+const onlyMarketing = document.querySelector('.only-marketing')
+fs.collection('users').onSnapshot((querySnapshot) => {
+  onlyMarketing.innerHTML = '';
+  querySnapshot.forEach((doc) => {
+    // console.log(`${doc.id} => ${doc.data().checkAdmin}`);
+    const userLogueado = firebase.auth().currentUser;
+
+    if (doc.id === userLogueado.uid) {
+      if (doc.data().area === 'GMM') {
+        onlyMarketing.innerHTML += `
+        <li><a href="#/" class="admin-web">Herramientas IMAX
+        <ul class="nav-admin-web hide">
+              <a href="projectsWeb.html" ><li class="btn-admin-boletas second-parent" >Agregar Noticia Web</li></a>
+              <a href="webAddProject.html" ><li class="btn-admi-const second-parent" >Agregar Proyecto </li></a>
+          </ul>
+        </a></li> `;
+        const menuAdminWeb = document.querySelector(".admin-web");
+        console.log(menuAdminWeb);
+        const navAdminWeb = document.querySelector(".nav-admin-web");
+        menuAdminWeb.addEventListener("click", () => {
+          navAdminWeb.classList.toggle("hide");
+          console.log("click");
+        });
+      }
+
+    }
+  })
+});
+
+const onlyLider = document.querySelector('.only-lider')
+fs.collection('users').onSnapshot((querySnapshot) => {
+  onlyLider.innerHTML = '';
+  querySnapshot.forEach((doc) => {
+    // console.log(`${doc.id} => ${doc.data().checkAdmin}`);
+    const userLogueado = firebase.auth().currentUser;
+
+    if (doc.id === userLogueado.uid) {
+      if (doc.data().checkLider === 'SI') {
+        onlyLider.innerHTML += `
+        <li><a href="vacationsLider.html">Lider de Unidad</a></li>`;
+        // const menuAdminWeb = document.querySelector(".admin-web");
+        // console.log(menuAdminWeb);
+        // const navAdminWeb = document.querySelector(".nav-admin-web");
+        // menuAdminWeb.addEventListener("click", () => {
+        //   navAdminWeb.classList.toggle("hide");
+        //   console.log("click");
+        // });
+      }
+
+    }
+  })
+});
+
+
+
+
 const userNew = document.querySelector('.user-new')
-userNew.innerHTML = ''; 
+userNew.innerHTML = '';
 fs.collection('users').onSnapshot((querySnapshot) => {
   querySnapshot.forEach((doc) => {
     // console.log(`${doc.id} => ${doc.data().checkAdmin}`);
     const userLogueado = firebase.auth().currentUser;
     const user = doc.data();
     if (doc.id === userLogueado.uid) {
-        userNew.innerHTML += `
+      userNew.innerHTML += `
         <p class="userLog"><i class="fas fa-user"></i>${user.name}</p>`;
-        userLogueado.updateProfile({
-          displayName: user.name,
-        }).then(function() {
-         console.log('correcto')
-         console.log(userLogueado)
-        });
+      userLogueado.updateProfile({
+        displayName: user.name,
+      }).then(function () {
+        console.log('correcto')
+        console.log(userLogueado)
+      });
 
-  
-      }
 
-    })
+    }
 
-  });
+  })
 
-  
+});
+
+
 
 //funcion para cerra sesion
 const logOut = document.querySelector('.logOut');
